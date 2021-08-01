8edcb8d5db3c632f08a4a44d755df331ca8a5077
Hossein Darvishi <hosseindarvishi138@gmail.com>
2021-08-01 16:20:50 +0430

    Add Utility Freamwork To sln Add GenerateResult Class in Freamwork

---
+++ UtilityFreamwork/GenerateResult.cs
@@ -0,0 +1,27 @@
+﻿namespace UtilityFreamwork
+{
+    public class GenerateResult
+    {
+        public bool IsSuccedded { get; set; }
+        public string Message { get; set; }
+
+        public GenerateResult()
+        {
+            IsSuccedded = true;
+        }
+
+        public GenerateResult Succedded(string message = "عملیات با موفقیت انجام شد.")
+        {
+            IsSuccedded = true;
+            Message = message;
+            return this;
+        }
+
+        public GenerateResult Failed(string message)
+        {
+            IsSuccedded = false;
+            Message = message;
+            return this;
+        }
+    }
+}
