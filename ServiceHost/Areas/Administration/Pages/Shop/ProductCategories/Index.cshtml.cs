948927e369d788b7323a7b0c9ac8b8bb9ee67bd2
Hossein Darvishi <hosseindarvishi138@gmail.com>
2021-08-02 17:32:00 +0430

    Creating Management ProductCategories Page

--- ServiceHost/Areas/Administration/Pages/Shop/ProductCategories/Index.cshtml.cs
+++ ServiceHost/Areas/Administration/Pages/Shop/ProductCategories/Index.cshtml.cs
@@ -4,13 +4,24 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ShopManagement.Application.Constract.ProductCategory;
 
 namespace ServiceHost.Areas.Administration.Pages.Shop.ProductCategories
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        private readonly IProductCategoryApplication categoryApplication;
+
+        public IndexModel(IProductCategoryApplication categoryApplication)
         {
+            this.categoryApplication = categoryApplication;
+        }
+
+        public List<ProductCategoryVM> categories;
+
+        public void OnGet(ProductCategorySearchModel search)
+        {
+            //categories = categoryApplication.Search(search);
         }
     }
 }
